4c3c49e64dd5efb9ef25f1999c3f4afe22566fd5
Carl Ollivier-Gooch <cfog@mech.ubc.ca>
2018-09-15 14:41:48 -0700

    Added GPL copyright and license notices.

--- surface_proj.cxx
+++ surface_proj.cxx
@@ -8,6 +8,26 @@
  *      by Carl Ollivier-Gooch.
  */
 
+/*
+    Copyright (C) The University of British Columbia, 2018.
+
+    This file is part of UnstructuredMeshAnalyzer.
+
+    UnstructuredMeshAnalyzer is free software: you can redistribute it
+    and/or modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation, either version 3 of
+    the License, or (at your option) any later version.
+
+    UnstructuredMeshAnalyzer is distributed in the hope that it will be
+    useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+    of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with UnstructuredMeshAnalyzer.  If not, see
+    <https://www.gnu.org/licenses/>.
+*/
+
 // GeodeProjectPoints.cpp
 //
 #include <string>
